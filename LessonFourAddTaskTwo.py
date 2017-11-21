4fbf23d3715291dd94816a1d2a94a10a4db75be3
esergly <29374586+esergly@users.noreply.github.com>
2017-11-22 01:39:33 +0200

    Update LessonFourAddTaskTwo.py

--- LessonFourAddTaskTwo.py
+++ LessonFourAddTaskTwo.py
@@ -1,3 +1,13 @@
+# Вариант 1
 my_list = [7, 2, 9, 4]
 my_list.reverse()
 print(my_list)
+# вывод: [4, 9, 2, 7]
+
+# Вариант 2
+my_list = [7, 2, 9, 4]
+size = len(my_list)
+for i in range(size):
+    my_list.append(my_list[(size-1)-i])
+print(my_list)
+# вывод: [7, 2, 9, 4, 4, 9, 2, 7]
